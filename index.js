614606bdaf39a15532c35e763c864b724abbad7a
rbrtrss <roberto.rousse@gmail.com>
2021-07-19 16:01:13 -0300

    primera version

---
+++ index.js
@@ -0,0 +1,26 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.end(createObject())
+});
+
+const randomEntreEnteros = (abajo, arriba) => {
+    return Math.floor(Math.random()*(arriba - abajo + 1) + abajo)
+}
+
+const randomEntreFloats = (abajo, arriba, decimales = 2) => {
+    return (Math.random()*(arriba - abajo + 1) + abajo).toFixed(decimales)
+}
+
+const createObject = () => {
+    return JSON.stringify({
+        id: randomEntreEnteros(1,10),
+        title: `Producto ${randomEntreEnteros(1,10)}`,
+        price: randomEntreFloats(0.0,9999.99),
+        thumbnail: `Foto ${randomEntreEnteros(1,10)}`
+    })
+}
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('servidor en 3000')
+}) 
